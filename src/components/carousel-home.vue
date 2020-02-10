8026ad42add98b7a72d481b23e5862eb9ef02957
Rolland <Rollandcampbell@msn.com>
2020-02-10 11:54:54 -0700

    styling home page

---
+++ src/components/carousel-home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="carousel-home offset-md-3 col-md-6">
+    <div id="carouselId" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselId" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselId" data-slide-to="1"></li>
+        <li data-target="#carouselId" data-slide-to="2"></li>
+        <li data-target="#carouselId" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+          <img class="homeCarouselPic" src="../assets/music.jpg" alt="First slide" />
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Music</h3>
+            <p>Introduce your child to the beauty of music.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="homeCarouselPic" src="../assets/dance.jpg" alt="Second slide" />
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Dance</h3>
+            <p>Help your child to be active, and expressive.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="homeCarouselPic" src="../assets/theatre.jpg" alt="Third slide" />
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Theatre</h3>
+            <p>Build your childs confidence and self-esteem.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="homeCarouselPic" src="../assets/gymnastics.jpg" alt="Fourth slide" />
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Gymnastics</h3>
+            <p>Build your childs strength.</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "carousel-home",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style scoped>
+.carousel-home {
+  padding-top: 2vh;
+}
+
+.carousel-inner {
+  border-radius: 20px;
+}
+
+.homeCarouselPic {
+  height: 50vh;
+  width: 100%;
+}
+
+p,
+h3 {
+  text-shadow: 1px 1px black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <div class="row">
+      <CarouselHome />
+    </div>
+    <div class="row">
+      <div
+        class="homeText col-md-8 offset-md-2"
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla perferendis illum nesciunt porro dolor ipsum reiciendis alias, temporibus aliquid beatae veritatis unde eius sed? Quae maiores dolores dolorum iusto quibusdam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui fugit quia similique corrupti amet temporibus reprehenderit maiores itaque quisquam consequuntur numquam quis, corporis ut aliquid repellendus quam ratione iusto soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repudiandae aliquam totam expedita voluptatibus eveniet necessitatibus rem commodi! Molestiae, aperiam et rem illo ab corrupti! Soluta nam iure ullam suscipit.</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import CarouselHome from "../components/carousel-home";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: { CarouselHome }
+};
 </script>
+
+<style scoped>
+.home {
+  height: 90vh;
+  background-color: hotpink;
+}
+
+.homeText {
+  color: white;
+  padding-top: 2vh;
+}
+</style>
